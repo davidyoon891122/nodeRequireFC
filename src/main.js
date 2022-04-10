0727b977907c83c43df439134c374153bee7d6c7
davidyoon <qqpo12@naver.com>
2022-04-10 19:37:47 +0900

    add mjs files to test ECMA import export

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// @ts-check
+// 1. 파일 간에 참조 방조 방법
+// 2. nodeJS에서 제공하는 스탠다드 라이브러리
+// 3. 컨벤션
+// 4. 데이터 구조
+// 5. 내장 객체
+// console.log(module.require('./animals'))
+const { path, paths, filename } = module
+
+console.log({
+  path,
+  paths,
+  filename,
+})
---
+++ src/main.mjs
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+import animals from './animals.mjs'
+import birds from './birds.js'
+
+console.log(birds)
+console.log(animals)
